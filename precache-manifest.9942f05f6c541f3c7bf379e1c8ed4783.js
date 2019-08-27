85a45f445f504f0935cda3f807046e6804abc830
mxk5025 <michaelkha213@gmail.com>
2019-08-27 19:36:01 -0400

    Updates

---
+++ precache-manifest.9942f05f6c541f3c7bf379e1c8ed4783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d3f6dd9bc787b486fa85d9875b68c8",
+    "url": "/Stock-Portfolio/index.html"
+  },
+  {
+    "revision": "29ecbd310a18f001fcef",
+    "url": "/Stock-Portfolio/static/css/main.5efc3ed5.chunk.css"
+  },
+  {
+    "revision": "a2700e7cc8e84f9271f4",
+    "url": "/Stock-Portfolio/static/js/2.076aca51.chunk.js"
+  },
+  {
+    "revision": "29ecbd310a18f001fcef",
+    "url": "/Stock-Portfolio/static/js/main.8f63eb57.chunk.js"
+  },
+  {
+    "revision": "c2a0a83ecbd6fec2e593",
+    "url": "/Stock-Portfolio/static/js/runtime~main.2d2a126e.js"
+  },
+  {
+    "revision": "ce9daa79e777f1f6bb58a40f88c8e09e",
+    "url": "/Stock-Portfolio/static/media/lodash.ce9daa79.svg"
+  },
+  {
+    "revision": "34f952f21f111111aa0d2a3065202825",
+    "url": "/Stock-Portfolio/static/media/logo-black.34f952f2.svg"
+  }
+]);
